1f046475897973edb192653694d73547286da946
NvG8956XD02 <gutenburg429@gmail.com>
2024-05-23 19:11:40 +0200

    Add more home-services

---
+++ guix/myr/home-services/games.scm
@@ -0,0 +1,17 @@
+(define-module (myr home-services games)
+  #:use-module (gnu home services)
+  #:use-module (gnu packages gnome)
+  #:use-module (nongnu packages game-client))
+
+(define (home-games-profile-service config)
+  (list steam
+        lutris))
+
+(define-public home-games-service-type
+  (service-type (name 'home-games)
+                (description "Packages and configuration for video games.")
+                (extensions
+                 (list (service-extension
+                        home-profile-service-type
+                        home-games-profile-service)))
+                (default-value #f)))
