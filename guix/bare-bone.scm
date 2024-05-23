ad180fdf61190087af617a9e5bc50a0275c8243a
NvG8956XD02 <gutenburg429@gmail.com>
2024-05-23 17:23:15 +0200

    Guix new Try

--- guix/bare-bone.scm
+++ guix/bare-bone.scm
@@ -15,44 +15,39 @@
   (timezone "Europe/Budapest")
   (locale "en_US.utf8")
 
+  (kernel-loadable-modules (list v4l2loopback-linux-module))
+
   (keyboard-layout (keyboard-layout "hu"))
   (bootloader (bootloader-configuration
                 (bootloader grub-efi-bootloader)
                 (targets '("/boot/efi"))
                 (keyboard-layout keyboard-layout)))
 
- (mapped-devices
-   (list (mapped-device
-           (source (uuid "12345678-1234-1234-1234-123456789abc"))
-           (target "crypt-root")
-           (type luks-device-mapping)))
-
-  (file-systems (append
-                 (list (file-system
+  (file-systems (cons* 
+		  (file-system
                          (device (file-system-label "guix-linux"))
                          (mount-point "/")
                          (type "btrfs")
-                         (options "noatime,space_cache=v2,compress=lzo,discard=async,subvol=@")
+                         (options "space_cache=v2,compress=lzo,discard=async,subvol=@")
 			 (dependencies mapped-devices))
 		       (file-system
                          (device (file-system-label "guix-linux"))
                          (mount-point "/home")
                          (type "btrfs")
-                         (options "relatime,space_cache=v2,compress=zstd,discard=async,subvol=@home")
+                         (options "space_cache=v2,compress=zstd,discard=async,subvol=@home")
 		         (dependencies mapped-devices))
 		       (file-system
                          (device (file-system-label "guix-linux"))
                          (mount-point "/gnu/store")
                          (type "btrfs")
-                         (options "noatime,space_cache,compress=lzo,discard=async,subvol=@gnu")
+                         (options "space_cache,compress=lzo,discard=async,subvol=@gnu")
 		         (dependencies mapped-devices))
                        (file-system
                          (device (uuid "1234-ABCD" 'fat))
                          (mount-point "/boot/efi")
-                         (type "vfat")))
+                         (type "vfat"))
                  %base-file-systems))
 
-  ;; Create user `bob' with `alice' as its initial password.
   (users (cons (user-account
                 (name "davy")
                 (comment "Davy Jones")
